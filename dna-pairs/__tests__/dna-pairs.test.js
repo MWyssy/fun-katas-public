11bb7dba1dfb53c403601e000c8a8ba3e65b929c
MWyssy <mike@wyss.co.uk>
2023-04-05 14:51:59 +0100

    add dna-pairs tests and solution

--- dna-pairs/__tests__/dna-pairs.test.js
+++ dna-pairs/__tests__/dna-pairs.test.js
@@ -1,7 +1,31 @@
 const dnaPairs = require('../dna-pairs.js');
 
-describe('dnaPairs()', () => {
-  test('ADD YOUR TEST DESCRIPTION HERE', () => {
-    // add your test assertion(s) here
+describe('dnaPairs tests', () => {
+    test('returns a nested array', () => {
+      //Arrange
+      const dnaString = '';
+      const expectedOutput = [[]];
+      //Act
+      const output = dnaPairs(dnaString);
+      //Assert
+      expect(output).toEqual(expectedOutput);
+    });
+    test('returns a nested array containing a single pair for a single dna strand', () => {
+      //Arrange
+      const dnaString = 'G';
+      const expectedOutput = [['G', 'C']];
+      //Act
+      const output = dnaPairs(dnaString);
+      //Assert
+      expect(output).toEqual(expectedOutput);
+    });
+    test('returns a nested array containing the pairs for all dna strands in the provided string', () => {
+      //Arrange
+      const dnaString = 'ATAG';
+      const expectedOutput = [["A", "T"], ["T", "A"], ["A", "T"], ["G", "C"]];
+      //Act
+      const output = dnaPairs(dnaString);
+      //Assert
+      expect(output).toEqual(expectedOutput);
+    });
   });
-});
--- dna-pairs/dna-pairs.js
+++ dna-pairs/dna-pairs.js
@@ -1,6 +1,38 @@
-// Please do not change the name of this function
 function dnaPairs(dna) {
-  // Write your code here
+  if (dna === undefined || dna.length === 0) return [[]];
+  
+  const result = [];
+  const dnaLetters = {
+    a: 'A', 
+    c: 'C', 
+    g: 'G', 
+    t: 'T',
+  }
+  const dnaArray = dna.split('');
+
+  for (let i = 0; i < dnaArray.length; i++) {
+    const pairArray = [];
+    const dnaStrand = dnaArray[i];
+  
+    switch (dnaStrand) {
+      case 'A': 
+        pairArray.push(dnaStrand, dnaLetters.t);
+        break;
+      case 'C': 
+        pairArray.push(dnaStrand, dnaLetters.g);
+        break;  
+      case 'G': 
+        pairArray.push(dnaStrand, dnaLetters.c);
+        break;
+      case 'T': 
+        pairArray.push(dnaStrand, dnaLetters.a);
+        break;  
+    }
+      
+    result.push(pairArray);
+  }
+
+  return result;
 }
 
 module.exports = dnaPairs;
